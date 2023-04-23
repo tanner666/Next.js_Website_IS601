4f66325d1b65d63395ed8f2ac9ef511004971384
tanner666 <tjg5@njit.edu>
2023-04-22 22:07:28 -0400

    added pop up component for privacy policy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,72 +1,32 @@
 //npm install -D tailwindcss@latest postcss@latest autoprefixer@latest
-const plugin = require('tailwindcss/plugin');
-const { blackA, mauve, violet, indigo, purple } = require('@radix-ui/colors');
+const { mauve, violet, red, blackA } = require('@radix-ui/colors');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./components/navbar.jsx'],
+  content: ['./components/pop-up.jsx'],
   theme: {
     extend: {
       colors: {
-        ...blackA,
         ...mauve,
         ...violet,
-        ...purple,
-        ...indigo,
+        ...red,
+        ...blackA,
       },
       keyframes: {
-        enterFromRight: {
-          from: { opacity: 0, transform: 'translateX(200px)' },
-          to: { opacity: 1, transform: 'translateX(0)' },
-        },
-        enterFromLeft: {
-          from: { opacity: 0, transform: 'translateX(-200px)' },
-          to: { opacity: 1, transform: 'translateX(0)' },
-        },
-        exitToRight: {
-          from: { opacity: 1, transform: 'translateX(0)' },
-          to: { opacity: 0, transform: 'translateX(200px)' },
-        },
-        exitToLeft: {
-          from: { opacity: 1, transform: 'translateX(0)' },
-          to: { opacity: 0, transform: 'translateX(-200px)' },
-        },
-        scaleIn: {
-          from: { opacity: 0, transform: 'rotateX(-10deg) scale(0.9)' },
-          to: { opacity: 1, transform: 'rotateX(0deg) scale(1)' },
-        },
-        scaleOut: {
-          from: { opacity: 1, transform: 'rotateX(0deg) scale(1)' },
-          to: { opacity: 0, transform: 'rotateX(-10deg) scale(0.95)' },
-        },
-        fadeIn: {
+        overlayShow: {
           from: { opacity: 0 },
           to: { opacity: 1 },
         },
-        fadeOut: {
-          from: { opacity: 1 },
-          to: { opacity: 0 },
+        contentShow: {
+          from: { opacity: 0, transform: 'translate(-50%, -48%) scale(0.96)' },
+          to: { opacity: 1, transform: 'translate(-50%, -50%) scale(1)' },
         },
       },
-    },
-    animation: {
-      scaleIn: 'scaleIn 200ms ease',
-      scaleOut: 'scaleOut 200ms ease',
-      fadeIn: 'fadeIn 200ms ease',
-      fadeOut: 'fadeOut 200ms ease',
-      enterFromLeft: 'enterFromLeft 250ms ease',
-      enterFromRight: 'enterFromRight 250ms ease',
-      exitToLeft: 'exitToLeft 250ms ease',
-      exitToRight: 'exitToRight 250ms ease',
+      animation: {
+        overlayShow: 'overlayShow 150ms cubic-bezier(0.16, 1, 0.3, 1)',
+        contentShow: 'contentShow 150ms cubic-bezier(0.16, 1, 0.3, 1)',
+      },
     },
   },
-  plugins: [
-    plugin(({ matchUtilities }) => {
-      matchUtilities({
-        perspective: (value) => ({
-          perspective: value,
-        }),
-      });
-    }),
-  ],
+  plugins: [],
 };
