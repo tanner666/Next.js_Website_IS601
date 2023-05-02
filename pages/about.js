ae4c5e66a89adaf4fe6e619424221495323afd7a
MSrikavya <machineni.srikavya4@gmail.com>
2023-05-02 11:28:32 -0400

    about page changes

--- pages/about.js
+++ pages/about.js
@@ -11,28 +11,30 @@ export default function Home({ allPostsData }) {
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className="text-gray-600 body-font">
+      <section className="text-white ">
         <div className="max-w-5xl pt-36 pb-24 mx-auto text-center">
-          <h1 className="text-80 font-4 lh-6 ld-04 font-bold text-white mb-6">
-            Welcome to MyWebClass
-          </h1>
-          <h2 className="text-2xl font-4 font-semibold lh-6 ld-04 pb-11 text-gray-700">
-            nine4 is a free to use template website for websites made
-            <br />
-            with Next.js and styled with Tailwind CSS
-          </h2>
-          <Link href="/about" className="inline-flex items-center py-3 px-14 font-semibold tracking-tighter text-white transition duration-500 ease-in-out transform bg-gradient-to-r from-blue-500 to-blue-800 text-md md:mt-0 focus:shadow-outline">
-            <div className="flex text-lg justify-center">
-              Learn More
-            </div>
-          </Link>
-        </div>
-        <div className="container flex flex-col items-center justify-center mx-auto">
-          <img
-            className="object-cover object-center w-3/4 mb-10 border shadow-md g327"
-            alt="Placeholder Image"
-            src="./images/placeholder.png"
-          />
+        <p>
+        At {siteTitle}, our mission is to empower teachers to teach web
+        development effectively and inspire students to become lifelong learners
+        in the field. We believe that everyone should have access to high-quality
+        web development education, regardless of their background or experience
+        level.
+      </p>
+      <p>
+        Our platform is designed to provide teachers with the resources they need
+        to create engaging and effective web development courses, and to support
+        them in their efforts to help their students succeed. We believe that by
+        fostering a community of dedicated teachers and learners, we can help to
+        create a world where anyone can learn the skills they need to thrive in
+        the digital age.
+      </p>
+      <p>
+        We are committed to staying up-to-date with the latest trends and
+        technologies in web development, and to continually improving our
+        platform to better serve the needs of teachers and students alike. Our
+        goal is to be the go-to resource for web development education, and to
+        help shape the future of this exciting and rapidly evolving field.
+      </p>
         </div>
       </section>
     </Layout>
