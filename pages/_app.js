745c46e30f102004ffbb744a3d336cb71894adaa
tanner666 <tjg5@njit.edu>
2023-05-07 21:47:41 -0400

    fixed text translation

--- pages/_app.js
+++ pages/_app.js
@@ -2,15 +2,23 @@
 import '../styles/global.css'
 import PrivacyPolicyPopup from '../components/privacy-pop-up';
 import '../i18n';
-import {useTranslation} from 'react-i18next'
-
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import { getTranslationData } from '../i18n';
 
 export default function App({ Component, pageProps }) {
-  const {t} = useTranslation();
+  const [translationData, setTranslationData] = useState({});
+  const router = useRouter();
+
+  useEffect(() => {
+    const locale = router.query.locale || 'en';
+    const data = getTranslationData(locale);
+    setTranslationData(data);
+  }, [router.query.locale]);
   
   return (
     <>
-      <h1>{t('welcome')}</h1>
+      <h1>{translationData.welcome}</h1>
       <Component {...pageProps} />
       <PrivacyPolicyPopup />
     </>
--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,20 @@ import Link from 'next/link'
 import Layout, { siteTitle } from '../components/layout'
 import { useEffect, useState } from 'react';
 import { getSortedPostsData } from '../lib/posts'
-import {useRouter} from 'next/router';
-import React from 'react';
-import { useTranslation } from 'react-i18next';
+import { useRouter } from 'next/router';
+import { getTranslationData } from '../i18n';
 
 export default function Home({ allPostsData }) {
-  const {t}  = useTranslation();
+  const [translationData, setTranslationData] = useState({});
+  const router = useRouter();
+
+  useEffect(() => {
+    const locale = router.query.locale || 'en';
+    const data = getTranslationData(locale);
+    setTranslationData(data);
+  }, [router.query.locale]);
   return (
     <>
-    <h1>{t('welcome')}</h1>
     <Layout>
       <Head>
         <title>MyWebClass.org: Agile & Lean Education for the AI-Driven World</title>
@@ -19,12 +24,12 @@ export default function Home({ allPostsData }) {
       <section className="body-font">
         <div className="max-w-6xl pt-12 pb-24 mx-auto text-center">
           <h1 className="text-80 lh-6 ld-04 font-bold title mb-6">
-            {t('welcome')}
+            {translationData.welcome}
           </h1>
           <h2 className="text-2xl font-semibold lh-6 ld-04 pb-11 subtext">
-            {t('description1')}
+            {translationData.description1}
             <br />
-            {t('description2')}
+            {translationData.description2}
           </h2>
           <Link href="/about/our-mission" className="inline-flex items-center py-3 px-14 font-semibold tracking-tighter text-white transition duration-500 ease-in-out transform bg-gradient-to-r from-blue-500 to-blue-800 text-md md:mt-0 focus:shadow-outline">
             <div className="flex text-lg justify-center">
