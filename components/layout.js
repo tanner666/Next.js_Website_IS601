e82eb82d203924be9db74cf2e14e457dff5247ca
tanner666 <tjg5@njit.edu>
2023-05-08 00:17:45 -0400

    added Google Analytics tag

--- components/layout.js
+++ components/layout.js
@@ -19,7 +19,7 @@ export default function Layout({ children, home }) {
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-TBPFJR2B7M"></script>
         <script>
           window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
+          function gtag(){dataLayer.push(arguments)}
           gtag('js', new Date());
 
           gtag('config', 'G-TBPFJR2B7M');
